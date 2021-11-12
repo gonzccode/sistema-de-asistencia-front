d0148282d15250d1bdad0d1d9281ab49d4d87397
GonzaCC <acanazac@uni.pe>
2021-11-11 20:05:15 -0500

    interfaces gonzalo

---
+++ pages/justificacion-alumno.vue
@@ -0,0 +1,140 @@
+<template>
+    
+    <div class="justificacion-alumno">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+        <div class="title-justificacion-alumno">
+            <div class="title-opcion-alumno">
+                <h2> <strong> <i class="bi bi-envelope-fill"></i> Justicación de Faltas</strong></h2>
+            </div>
+            <div class="foto-alumno">
+                <img src="@/assets/images/estudiante-perfil.jpg" height="60em">
+            </div>
+
+        </div>
+        <div class="lista-justificacion-alumno">
+            <h3><strong>Lista de Faltas</strong> </h3>
+            <div class="table-justificacion-alumno" align="center">
+                <table class="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th>Fecha</th>
+                        <th>Tipo de Falta</th>
+                        <th>Estado</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>20/09/2021</td>
+                        <td>Inasistencia</td>
+                        <td style="color: #22be34"><strong> Justificación aceptada</strong> </td>
+                    </tr>
+                    <tr>
+                        <td>10/09/2021</td>
+                        <td>Tardanza</td>
+                        <td style="color: #22be34"> <strong> Justificación aceptada</strong></td>
+                    </tr>
+                    <tr>
+                        <td>05/09/2021</td>
+                        <td>Tardanza</td>
+                        <td style="color: #22be34"> <strong> Justificación aceptada</strong></td>
+                    </tr>
+                    <tr>
+                        
+                        <td>01/09/2021</td>
+                        <td>Inasistencia</td>
+                        <td> <button  type="button" class="btn btn-danger btn-sm" data-bs-toggle="modal" data-bs-target="#exampleModal">No justificado</button> </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+        <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog">
+                <div class="modal-content">
+                <div class="modal-header bg-primaary">
+                    <h5 class="modal-title" id="exampleModalLabel">Justificación de Falta</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form class="form-justificacion">
+                <div class="modal-body">
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label"><strong>Descripción</strong> </label>
+                            <textarea class="form-control" id="message-text" placeholder="Escriba justificación"></textarea>
+                            
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputPassword1" class="form-label"><strong>Adjuntar documento</strong> </label>
+                            <!--<input type="text" class="form-control" id="exampleInputPassword1" placeholder="Cargue su documento">-->
+                            <div class="input-group mb-3">
+                                <input type="file" class="form-control" id="inputGroupFile02" placeholder="Cargue su documento">
+                                <!--<label class="input-group-text" for="inputGroupFile02">Upload</label>-->
+                            </div>
+                        </div>
+                        <!--<button type="submit" class="btn btn-primary">Aceptar</button>
+                        <button type="submit" class="btn btn-primary">Rechazar</button>-->
+                   
+                </div>
+                <div class="modal-footer">
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                    
+                </div>
+                </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</template>
+
+<script lang="ts">
+    import { Vue } from "nuxt-property-decorator";
+    
+
+    export default class extends Vue {
+        showModal = false
+
+    }
+</script>
+
+<style>
+    .justificacion-alumno {
+        margin:0 auto;
+        
+    }
+
+    .title-justificacion-alumno{
+        margin:20px;
+        display: flex;
+    }
+
+    .title-opcion-alumno {
+        width: 93%;
+        margin-top: 7px;
+        
+    }
+
+    .foto-alumno{
+        width: 7%;
+        text-align: center;
+        
+    }
+    .foto-alumno img{
+        border: 1px solid #0000;
+    }
+
+    .lista-justificacion-alumno{
+        text-align: center;
+        margin:20px
+    }
+
+    .table-justificacion-alumno{
+        width: 80%;
+        margin: 0 auto;
+        margin-top: 20px;
+        text-align: center;
+    }
+        
+
+</style>
